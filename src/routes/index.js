7dce257b43f75c1c034bef07114ab249c5af5e00
elyeandre <96169920+elyeandre@users.noreply.github.com>
2024-09-18 18:39:40 +0800

    test: add / endpoint for JSON response testing

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,18 +3,9 @@ const router = express.Router();
 const path = require('path');
 
 let routes = (app) => {
-  // Serve the homepage
+  // Testing
   router.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../../public/index.html'));
-  });
-
-  // Serve the sign-up page
-  router.get('/signup', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../../public/html/signUp.html'));
-  });
-
-  router.get('/signin', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../../build/signin.html'));
+    res.json({ message: 'Hello, world!' });
   });
 
   app.use(router);
