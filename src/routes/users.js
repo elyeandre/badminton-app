39163e37c7b98d384de404bb53af43c7ca1a9883
elyeandre <96169920+elyeandre@users.noreply.github.com>
2024-09-22 23:18:02 +0800

    feat: add MongoDB connection check to /login route

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const mongoose = require('mongoose');
+const createError = require('http-errors');
 
 let routes = (app) => {
   // Serve the registration page
@@ -9,8 +11,18 @@ let routes = (app) => {
   });
 
   // Serve the login page
-  router.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../../build/signin.html'));
+  router.get('/login', async (req, res, next) => {
+    // check if MongoDB is connected
+    if (mongoose.connection.readyState !== 1) {
+      // 1 indicates connected
+      return next(createError(503, 'Service Unavailable.'));
+    }
+
+    try {
+      res.sendFile(path.resolve(__dirname, '../../build/signin.html'));
+    } catch (err) {
+      next(createError(500, 'Internal Server Error'));
+    }
   });
 
   app.use('/', router);
