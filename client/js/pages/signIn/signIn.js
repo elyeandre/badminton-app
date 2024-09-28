ab9b65612fb761d1c84dd5e0eac64f2bef6c2319
elyeandre <96169920+elyeandre@users.noreply.github.com>
2024-09-29 00:53:51 +0800

    feat: Implement login form handling and user type redirection
    - Added user type check to redirect to admin dashboard or home page based on selection for testing

--- client/js/pages/signIn/signIn.js
+++ client/js/pages/signIn/signIn.js
@@ -7,14 +7,59 @@ const getById = (id) => doc.getElementById(id);
 const getAll = (selector) => doc.querySelectorAll(selector);
 const get = (selector) => doc.querySelector(selector);
 
-function handleSubmit(event) {
-  event.preventDefault();
-  var userType = document.querySelector('select[name="userType"]').value;
-  if (userType === 'Admin') {
-    window.location.href = 'adminDash.html';
-  } else if (userType === 'Coach' || userType === 'Player') {
-    window.location.href = 'home.html';
-  } else {
-    alert('Please select a user type.');
+doc.addEventListener('DOMContentLoaded', function () {
+  const loginForm = getById('loginform');
+
+  loginForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const username = formData.get('username');
+    const password = formData.get('password');
+    const userType = formData.get('userType');
+
+    // attempt to log in
+    const success = await sendLoginRequest(username, password, userType);
+    if (!success) {
+      log('Login failed');
+    }
+  });
+});
+
+const sendLoginRequest = async (username, password, role) => {
+  log(username, password, role);
+  try {
+    const response = await fetch('/user/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username,
+        password,
+        role
+      })
+    });
+
+    const result = await response.json();
+
+    if (response.status === 200 && result.success) {
+      // handle different actions based on the response from the backend
+      if (result.action === 'verify') {
+        // redirect to verification page
+        window.location.href = result.verificationUrl;
+      } else if (result.action === 'redirect') {
+        // redirect to the role-specific page
+        window.location.href = result.redirectUrl;
+      }
+    } else {
+      // show an error message to the user
+      alert(result.message);
+      return false;
+    }
+  } catch (err) {
+    error('Error logging in:', err);
+    alert('An error occurred while logging in. Please try again.');
+    return false;
   }
-}
+};
