092976eb739150d5fe8508893328b7e86964350e
elyeandre <96169920+elyeandre@users.noreply.github.com>
2024-09-29 23:59:46 +0800

    feat: add environment variables for email and JWT configuration

--- config/commonConfig.js
+++ config/commonConfig.js
@@ -13,6 +13,7 @@ module.exports = {
     pass: process.env.EMAIL_PASSWORD
   },
   jwtSecret: process.env.JWT_SECRET,
+  jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
   port: process.env.PORT || 3000,
   disableSecurity: process.env.DISABLE_SECURITY
 };
