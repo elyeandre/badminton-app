31c097d354800836d1a83b40f92cbc139d489113
elyeandre <96169920+elyeandre@users.noreply.github.com>
2024-10-02 10:56:52 +0800

    refactor(controller): combine user-related functions into a single controller file

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,13 +1,12 @@
-const createError = require('http-errors');
 const User = require('../models/User');
 const { log, error } = console;
-const { generateNewVerificationToken } = require('../utils/genVerificationToken');
-const { generateAccessAndRefreshTokens } = require('../utils/generateToken');
+const { generateAccessAndRefreshTokens, generateNewVerificationToken } = require('../utils/generateToken');
+const { addToBlacklist, isTokenBlacklisted } = require('../utils/blackListUtils');
 const config = require('config');
 const jwt = require('jsonwebtoken');
 const { Buffer } = require('buffer');
-const { sendOTP } = require('../services/emailService');
-const Blacklist = require('../models/Blacklist');
+const { sendOTP, sendForgotPasswordEmail } = require('../services/emailService');
+const crypto = require('crypto');
 
 exports.loginUser = async (req, res, next) => {
   try {
@@ -55,7 +54,6 @@ exports.loginUser = async (req, res, next) => {
     // if the user is not verified, send verification data
     if (!user.isVerified) {
       const token = await generateNewVerificationToken(user.email); // generate a new verification token
-
       // send OTP
       await sendOTP(user.email);
 
@@ -194,15 +192,99 @@ exports.registerUser = async (req, res, next) => {
   }
 };
 
+exports.deleteAccount = async (req, res) => {
+  try {
+    // Get the session cookie or Authorization header
+    const authHeader = req.headers['cookie'];
+
+    // If neither are present, send an Unauthorized error
+    if (!authHeader) return res.sendStatus(401);
+
+    let accessToken, refreshToken;
+
+    // extract the token from the correct cookie name
+    if (req.headers['cookie']) {
+      // Split the cookie string into key-value pairs
+      const cookies = authHeader.split(';').reduce((acc, cookie) => {
+        const [name, value] = cookie.trim().split('=');
+        acc[name] = value;
+        return acc;
+      }, {});
+
+      accessToken = cookies['accessToken'];
+      refreshToken = cookies['refreshToken'];
+    }
+
+    // check if the access token is blacklisted
+    const isAccessTokenBlacklisted = await isTokenBlacklisted(accessToken, 'access');
+    const isRefreshTokenBlacklisted = await isTokenBlacklisted(refreshToken, 'refresh');
+
+    // if either the access token or the refresh token is blacklisted, send a no content response
+    if (isAccessTokenBlacklisted || isRefreshTokenBlacklisted) {
+      return res.sendStatus(403); // tokens are blacklisted, proceed with no content response
+    }
+
+    if (accessToken) {
+      // otherwise blacklist token
+      await addToBlacklist(accessToken, 'access');
+    }
+
+    // blacklist refresh token (if present)
+    if (refreshToken) {
+      await addToBlacklist(refreshToken, 'refresh');
+    }
+
+    // assume the user's ID is stored in req.user after authentication
+    const userId = req.user._id;
+
+    // find the user by ID and delete the account
+    const deletedUser = await User.findByIdAndDelete(userId);
+
+    // check if the user was found and deleted
+    if (!deletedUser) {
+      return res.status(404).json({
+        success: false,
+        code: 404,
+        message: 'User not found'
+      });
+    }
+
+    let options = {
+      httpOnly: true, // the cookie is only accessible by the web server
+      secure: true,
+      sameSite: 'Strict'
+    };
+
+    // clear request cookie on client
+    return res
+      .status(200)
+      .setHeader('Clear-Site-Data', '"cookies"')
+      .clearCookie('accessToken', options)
+      .clearCookie('refreshToken', options)
+      .json({
+        success: true,
+        code: 200,
+        message: 'Account deleted sucessfully'
+      });
+  } catch (err) {
+    error('Error occurred while deleting the account:', err);
+    return res.status(500).json({
+      status: 'error',
+      code: 500,
+      message: 'Internal Server Error'
+    });
+  }
+};
+
 exports.logoutUser = async (req, res, next) => {
   try {
     // Get the session cookie or Authorization header
-    const authHeader = req.headers['cookie'] || req.header('Authorization')?.replace('Bearer ', '');
+    const authHeader = req.headers['cookie'];
 
     // If neither are present, send an Unauthorized error
-    if (!authHeader) return res.sendStatus(204);
+    if (!authHeader) return res.sendStatus(401);
 
-    let token;
+    let accessToken, refreshToken;
 
     // If it's from the cookie, extract the token from the correct cookie name
     if (req.headers['cookie']) {
@@ -213,28 +295,30 @@ exports.logoutUser = async (req, res, next) => {
         return acc;
       }, {});
 
-      token = cookies['accessToken'];
+      accessToken = cookies['accessToken'];
+      refreshToken = cookies['refreshToken'];
     }
 
-    const checkIfBlacklisted = await Blacklist.findOne({ token: token }); // check if that token is blacklisted
+    // check if the access token is blacklisted
+    const isAccessTokenBlacklisted = await isTokenBlacklisted(accessToken, 'access');
+    const isRefreshTokenBlacklisted = await isTokenBlacklisted(refreshToken, 'refresh');
 
-    // if true, send a no content response.
-    if (checkIfBlacklisted) return res.sendStatus(204);
+    // if either the access token or the refresh token is blacklisted, send a no content response
+    if (isAccessTokenBlacklisted || isRefreshTokenBlacklisted) {
+      return res.sendStatus(403); // tokens are blacklisted, proceed with no content response
+    }
 
     // otherwise blacklist token
-    const decoded = jwt.verify(token, config.jwtSecret);
-    const expiresAt = new Date(decoded.exp * 1000); // convert exp to Date
+    if (accessToken) {
+      await addToBlacklist(accessToken, 'access');
+    }
 
-    // add token to blacklist
-    const blacklistEntry = new Blacklist({
-      token,
-      expiresAt
-    });
-
-    await blacklistEntry.save();
+    // blacklist refresh token (if present)
+    if (refreshToken) {
+      await addToBlacklist(refreshToken, 'refresh');
+    }
 
     // find the user and remove the refresh token
-    log(req.user._id);
     await User.findByIdAndUpdate(
       req.user._id,
       {
@@ -325,7 +409,6 @@ exports.verifyEmail = async (req, res, next) => {
     return res.status(200).json({
       success: true,
       code: 200,
-      data: [],
       message: 'Email verified sucessfully'
     });
   } catch (err) {
@@ -352,6 +435,16 @@ exports.refreshToken = async (req, res, next) => {
   }
 
   try {
+    // check if the refresh token is blacklisted
+    const isRefreshTokenBlacklisted = await isTokenBlacklisted(incomingRefreshToken, 'refresh');
+    if (isRefreshTokenBlacklisted) {
+      return res.status(403).json({
+        success: false,
+        code: 403,
+        message: 'Refresh token is blacklisted'
+      });
+    }
+
     // verify the refresh token
     const decodedToken = jwt.verify(incomingRefreshToken, config.jwtRefreshSecret);
 
@@ -366,11 +459,26 @@ exports.refreshToken = async (req, res, next) => {
       return res.status(401).json({ message: 'Refresh token is incorrect' });
     }
 
+    const incomingAccessToken = req.cookies.accessToken || req.body.accessToken;
+
+    // check if the access token is blacklisted
+    const isAccessTokenBlacklisted = await isTokenBlacklisted(incomingAccessToken, 'access');
+    if (!isAccessTokenBlacklisted) {
+      if (incomingAccessToken) {
+        await addToBlacklist(incomingAccessToken, 'access');
+      }
+    }
+
     let options = {
       httpOnly: true, // the cookie is only accessible by the web server
       secure: true,
       sameSite: 'Strict'
     };
+
+    if (incomingRefreshToken) {
+      // blacklist the old refresh token (optional)
+      await addToBlacklist(incomingRefreshToken, 'refresh');
+    }
 
     // generate access token and refresh token for user
     const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(decodedToken?.id);
@@ -390,6 +498,110 @@ exports.refreshToken = async (req, res, next) => {
     error('Error occurred while creating token:', err);
     return res.status(500).json({
       status: 'error',
+      code: 500,
+      message: 'Internal Server Error'
+    });
+  }
+};
+
+exports.resetPassword = async (req, res) => {
+  const { token, email, newPassword } = req.body;
+
+  // Validate input
+  if (!token || !email || !newPassword) {
+    return res.status(400).json({
+      success: false,
+      code: 400,
+      message: 'Token, email, and new password are required'
+    });
+  }
+
+  try {
+    // Decode the base64 email
+    const decodedEmail = Buffer.from(email, 'base64').toString();
+
+    // Find user by email and verify token
+    const user = await User.findOne({
+      email: decodedEmail,
+      resetPasswordToken: token,
+      resetPasswordExpires: { $gt: Date.now() } // Check if the token has not expired
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        code: 400,
+        message: 'Invalid or expired token'
+      });
+    }
+
+    // Update user's password
+    user.password = newPassword; // Ensure password is hashed in your User model
+    user.resetPasswordToken = undefined; // Clear the reset token
+    user.resetPasswordExpires = undefined; // Clear the expiration
+    await user.save();
+
+    return res.status(200).json({
+      success: true,
+      code: 200,
+      message: 'Password has been reset successfully'
+    });
+  } catch (err) {
+    error('Error during reset password:', err);
+    return res.status(500).json({
+      success: false,
+      code: 500,
+      message: 'Internal Server Error'
+    });
+  }
+};
+
+exports.forgotPassword = async (req, res) => {
+  const { email: encodedEmail } = req.body;
+
+  // decode Base64 email
+  const decodedEmail = Buffer.from(encodedEmail, 'base64').toString();
+
+  // check if email is provided
+  if (!decodedEmail) {
+    return res.status(400).json({
+      success: false,
+      code: 400,
+      message: 'Email is required'
+    });
+  }
+
+  try {
+    // find user by email
+    const user = await User.findOne({ email: decodedEmail });
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        code: 404,
+        message: 'User not found'
+      });
+    }
+
+    // generate a unique reset token
+    const resetToken = crypto.randomBytes(32).toString('hex');
+
+    // set token and expiration in user document
+    user.resetPasswordToken = resetToken;
+    user.resetPasswordExpires = Date.now() + 3600000; // 1 hour expiration
+    await user.save();
+
+    // Send email with the reset link
+    await sendForgotPasswordEmail(decodedEmail, resetToken);
+
+    return res.status(200).json({
+      success: true,
+      code: 200,
+      message: 'Reset password link has been sent to your email.'
+    });
+  } catch (err) {
+    error('Error during forgot password:', err);
+    return res.status(500).json({
+      success: false,
       code: 500,
       message: 'Internal Server Error'
     });
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const checkMongoConnection = require('../middleware/checkMongoConnection');
-const { validateRegistration } = require('../middleware/userRegValidator');
-const { validateLogin } = require('../middleware/loginValidator');
-const { validateVerify } = require('../middleware/userVerifyValidator');
+const {
+  validateVerify,
+  validateLogin,
+  validateRegistration,
+  validateForgotPassword
+} = require('../middleware/validator');
 const verifyToken = require('../middleware/authJwt');
-
-const { loginUser, logoutUser, refreshToken, registerUser, verifyEmail } = require('../controllers/authController');
+const {
+  loginUser,
+  logoutUser,
+  refreshToken,
+  registerUser,
+  verifyEmail,
+  deleteAccount,
+  forgotPassword,
+  resetPassword
+} = require('../controllers/authController');
 
 let routes = (app) => {
   // handle the verification
@@ -21,8 +32,17 @@ let routes = (app) => {
   // handle logout
   router.post('/logout', checkMongoConnection, verifyToken, logoutUser);
 
-  // handle logout
+  // handle refresh token
   router.post('/refresh', checkMongoConnection, refreshToken);
+
+  // handle account deletion
+  router.post('/delete', checkMongoConnection, verifyToken, deleteAccount);
+
+  // handle account forgot-password
+  router.post('/forgot-password', checkMongoConnection, validateForgotPassword, forgotPassword);
+
+  // handle account forgot-password
+  router.post('/reset-password', checkMongoConnection, validateForgotPassword, resetPassword);
 
   app.use('/auth', router);
 };
