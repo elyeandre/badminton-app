5335f37a2a7cff565c19bf72fda8875441b76b9d
elyeandre <96169920+elyeandre@users.noreply.github.com>
2024-10-02 10:21:29 +0800

    refactor(tokens): consolidate token generation functions into one file

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,4 +1,6 @@
+const CryptoJS = require('crypto-js');
 const User = require('../models/User');
+
 /**
  * Generates access and refresh tokens for a user and saves the refresh token in the database.
  * @param {String} userId - The ID of the user for whom to generate tokens.
@@ -28,6 +30,29 @@ const generateAccessAndRefreshTokens = async (userId) => {
   }
 };
 
+/**
+ * Generates a new verification token for the user.
+ * @param {string} email - The user's email.
+ * @returns {Promise<string>} - A promise that resolves with the new token after it's updated in the database.
+ */
+const generateNewVerificationToken = async (email) => {
+  const randomBytes = CryptoJS.lib.WordArray.random(32); // Generate 32 random bytes
+  const token = randomBytes.toString(); // Convert to a string
+
+  await User.updateOne(
+    { email },
+    {
+      verificationToken: token,
+      tokenExpires: Date.now() + 3600000, // 1-hour expiry
+      isTokenUsed: false // Ensure the token is marked as unused
+    }
+  );
+
+  return token; // Return the generated token
+};
+
+// Export both functions for use in other parts of your application
 module.exports = {
-  generateAccessAndRefreshTokens
+  generateAccessAndRefreshTokens,
+  generateNewVerificationToken
 };
