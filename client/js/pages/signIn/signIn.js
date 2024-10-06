c4e4a8aa0602b07ef16cc2a0d157438e2496d00a
elyeandre <96169920+elyeandre@users.noreply.github.com>
2024-10-06 22:00:41 +0800

    refactor: enhance signIn.js for improved readability and error handling

--- client/js/pages/signIn/signIn.js
+++ client/js/pages/signIn/signIn.js
@@ -6,10 +6,87 @@ const { log, error } = console;
 const getById = (id) => doc.getElementById(id);
 const getAll = (selector) => doc.querySelectorAll(selector);
 const get = (selector) => doc.querySelector(selector);
+const loginBtn = getById('loginBtn');
 
 doc.addEventListener('DOMContentLoaded', function () {
-  const loginForm = getById('loginform');
+  const modal = getById('modal');
+  const openModalButton = getById('open-modal');
+  const closeModalButton = getById('close-modal');
 
+  openModalButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    modal.style.display = 'flex';
+  });
+
+  closeModalButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    modal.style.display = 'none';
+  });
+
+  // forgot Password Form Submission
+  const forgotPasswordForm = getById('forgotPasswordForm');
+  const forgotSubmitBtn = getById('forgotSubmitBtn');
+  forgotPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const email = getById('verification-email').value.trim();
+    const emailError = getById('verification-email-error');
+
+    if (!email) {
+      log('Please enter a valid email.');
+      emailError.innerText = 'Email is required';
+      emailError.classList.add('show');
+
+      setTimeout(() => {
+        emailError.classList.remove('show');
+      }, 2000);
+
+      return;
+    }
+    forgotSubmitBtn.disabled = true;
+
+    // encode email in Base64
+    const encodedEmail = btoa(email); // Base64 encoding
+
+    try {
+      const response = await fetch('/auth/forgot-password', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: encodedEmail
+        })
+      });
+
+      const result = await response.json();
+      if (response.status === 200 && result.success) {
+        forgotSubmitBtn.disabled = false;
+        log('Reset link sent successfully:', result.message);
+        alert('A password reset link has been sent to your email.');
+        modal.style.display = 'none'; // close modal after successful request
+      } else {
+        forgotSubmitBtn.disabled = false;
+        log('Failed to send reset link:', result.message);
+        emailError.innerText = result.message;
+        emailError.classList.add('show');
+
+        setTimeout(() => {
+          emailError.classList.remove('show');
+        }, 2000);
+      }
+    } catch (err) {
+      forgotSubmitBtn.disabled = false;
+      error('Error sending reset link:', err);
+      emailError.innerText = 'An error occurred. Please try again.';
+      emailError.classList.add('show');
+
+      setTimeout(() => {
+        emailError.classList.remove('show');
+      }, 2000);
+    }
+  });
+
+  const loginForm = getById('loginform');
   loginForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
@@ -19,15 +96,13 @@ doc.addEventListener('DOMContentLoaded', function () {
     const userType = formData.get('userType');
 
     // attempt to log in
-    const success = await sendLoginRequest(username, password, userType);
-    if (!success) {
-      log('Login failed');
-    }
+    loginBtn.disabled = true;
+    await sendLoginRequest(username, password, userType);
   });
 });
 
 const sendLoginRequest = async (username, password, role) => {
-  log(username, password, role);
+  const loginError = getById('login-errors');
   try {
     const response = await fetch('/auth/login', {
       method: 'POST',
@@ -52,14 +127,50 @@ const sendLoginRequest = async (username, password, role) => {
         // redirect to the role-specific page
         window.location.href = result.redirectUrl;
       }
+      loginBtn.disabled = false;
     } else {
       // show an error message to the user
-      alert(result.message);
-      return false;
+      if (result.message && result.message === 'User not found.') {
+        loginError.innerText = 'User not found';
+        loginError.classList.add('show');
+
+        setTimeout(() => {
+          loginError.classList.remove('show');
+        }, 2000);
+      } else if (result.message && result.message === 'Invalid username or password') {
+        loginError.innerText = 'Invalid username or password';
+        loginError.classList.add('show');
+
+        setTimeout(() => {
+          loginError.classList.remove('show');
+        }, 2000);
+      } else if (result.message && result.message === 'User role does not match the specified role') {
+        loginError.innerText = 'User role does not match the specified role';
+        loginError.classList.add('show');
+
+        setTimeout(() => {
+          loginError.classList.remove('show');
+        }, 2000);
+      } else {
+        loginError.innerText = 'An error occured while logging in.';
+        loginError.classList.add('show');
+
+        setTimeout(() => {
+          loginError.classList.remove('show');
+        }, 2000);
+      }
+
+      loginBtn.disabled = false;
     }
   } catch (err) {
+    loginBtn.disabled = false;
     error('Error logging in:', err);
-    alert('An error occurred while logging in. Please try again.');
+    loginError.innerText = 'An error occured while logging in.';
+    loginError.classList.add('show');
+
+    setTimeout(() => {
+      loginError.classList.remove('show');
+    }, 2000);
     return false;
   }
 };
