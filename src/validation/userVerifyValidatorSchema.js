06e6e7ccb5cff0d9f8a93e26680111818c9b0bd1
elyeandre <96169920+elyeandre@users.noreply.github.com>
2024-10-12 08:58:56 +0800

    refactor: remove token validation

--- src/validation/userVerifyValidatorSchema.js
+++ src/validation/userVerifyValidatorSchema.js
@@ -1,9 +1,6 @@
 const Joi = require('joi');
 
 const verifySchema = Joi.object({
-  token: Joi.string().required().messages({
-    'any.required': 'Token is required.'
-  }),
   otp: Joi.string().length(6).required().messages({
     'string.length': 'OTP must be exactly 6 digits long.',
     'any.required': 'OTP is required.'
