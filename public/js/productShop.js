7f4c539fe37510bf2ecf3aae4e1bc2846cdbb804
frootyjas <jasminedenriquez@gmail.com>
2024-10-22 19:46:42 +0800

    add archive

--- public/js/productShop.js
+++ public/js/productShop.js
@@ -18,3 +18,23 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     };
 });
+
+function toggleTables() {
+    const productTableContainer = document.getElementById('productTableContainer');
+    const archiveTableContainer = document.getElementById('archiveTableContainer');
+    const toggleButton = document.getElementById('toggleTableButton');
+
+    // Check the current visibility of the product table container
+    if (productTableContainer.style.display === 'none') {
+        // Show product table container, hide archive table container, change button text to 'Archive'
+        productTableContainer.style.display = 'block';
+        archiveTableContainer.style.display = 'none';
+        toggleButton.innerHTML = 'Archive <i class="fas fa-archive"></i>';
+    } else {
+        // Show archive table container, hide product table container, change button text to 'Product List'
+        productTableContainer.style.display = 'none';
+        archiveTableContainer.style.display = 'block';
+        toggleButton.innerHTML = 'Product List <i class="fas fa-box"></i>';
+    }
+}
+
